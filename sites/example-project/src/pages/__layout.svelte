9fe43bd4af0897580836baf9740bb2215ff3b8c4
Adam <mcrascal@users.noreply.github.com>
2021-11-29 09:57:34 -0500

    rough in grid layout

--- sites/example-project/src/pages/__layout.svelte
+++ sites/example-project/src/pages/__layout.svelte
@@ -1,43 +1,187 @@
 <script>
 	import "../app.css"
 	import { navigating } from '$app/stores';
-	import { blur } from "svelte/transition"
-	import Nav from '@evidence-dev/components/ui/Nav.svelte';
-	import BreadCrumbs from '@evidence-dev/components/ui/BreadCrumbs.svelte';
 	import LoadingIndicator from '@evidence-dev/components/ui/LoadingIndicator.svelte';
-	import Logo from '@evidence-dev/components/ui/Logo.svelte';
-
+	import { blur, fade } from "svelte/transition"
+	import Logo from "@evidence-dev/components/ui/Logo.svelte";
+	import BreadCrumbs from "@evidence-dev/components/ui/BreadCrumbs.svelte";
 </script>
 
 <svelte:head>
 	<title>Evidence</title>
 </svelte:head>
 
-{#if $navigating}
-	<LoadingIndicator/>
-{/if}
+<!-- Two states: mobile open -->
 
-<div class="header">
-	<Logo organization="Austin 311"/>
-	<Nav
-		sections = {[
-			{href:"/", label: "Home"},
-			{href:"/departments", label: "Departments"}
-		]}
-	/>
-</div>
 
-{#if !$navigating}
-	<main in:blur|local>
-		<BreadCrumbs/>
-		<article>
-			<slot></slot>
-		</article>      
+	
+<div class="grid">
+	<header>
+	  <BreadCrumbs/>
+	</header>
+	<aside class=sidebar>
+	  <div class="sticky">
+		<h3><a href = '/' >Organization Name</a></h3>
+		<nav>
+		  <ul>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+			<li>Nav Item</li>
+		  </ul>
+		</nav>
+		<div class=side-bar-bottom>
+			<h1>Bottom stuff</h1>
+		</div>
+	  </div>
+	</aside>
+	<main>
+	{#if !$navigating}
+	  <article transition:fade>
+		  <slot/>
+	  </article>
+	{/if}
 	</main>
-{/if}
+	<aside class='toc'>
+		<div class="sticky">
+			<h4>On this page</h4>
+			  <ul>
+				<li>Nav Item</li>
+				<li>Nav Item</li>
+				<li>Nav Item</li>
+				<li>Nav Item</li>
+				<li>Nav Item</li>
+				<li>Nav Item</li>
+				<li>Nav Item</li>
+				<li>Nav Item</li>
+				<li>Nav Item</li>
+			  </ul>
+	</aside>
+  </div>
 
 <style>
-	div.header {
-		padding: .5em 1.5em 0 1.5em;
-	}
+.grid {
+  --header-height: 2em;
+  display: grid;
+  grid-template-areas:
+    'sidebar header header'
+    'sidebar main toc';
+  grid-template-columns: 14rem 1fr 14rem;
+  gap: 0 16px;
+  max-width: 1200px;
+  margin: 0 auto;
+  isolation: isolate;
+}
+
+header {
+  grid-area: header;
+  position: sticky;
+  z-index: 2;
+  top: 0;
+  display: grid;
+  place-content: center;
+  height: var(--header-height);
+  /* border-bottom: 3px solid; */
+  background-color: rgba(255, 255, 255, 0.95);
+}
+
+/*
+  The “aside” child is positioned according to
+  grid layout, filling the assigned grid area.
+*/
+aside.sidebar {
+  grid-area: sidebar;
+  position: relative;
+  z-index: 1;
+  padding-top: 0;
+  border: 3px solid;
+
+}
+
+.sticky {
+	position: sticky;
+  	top: var(--header-height);
+	margin-top: 0;
+	padding-top: 0;
+}
+
+nav {
+	max-height: 75vh;
+	overflow-y: scroll;
+}
+
+main {
+  grid-area: main;
+  border: 3px solid;
+}
+
+aside.toc {
+	grid-area: toc;
+	border: 3px solid;
+}
+
 </style>
